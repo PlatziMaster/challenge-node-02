32db8f822b8572fae39a17341f0d70c5a8477257
Iván D. Sánchez <cosmosoft.soluciones@gmail.com>
2020-03-16 23:42:40 -0500

    Primer version de scrapping platzirank

--- src/index.js
+++ src/index.js
@@ -1,24 +1,64 @@
 const puppeteer = require('puppeteer');
 
-const getDataFromPuppeteer = async () => {
+let urls = [
+  'https://platzi.com/@gndx/', 
+  'https://platzi.com/@cosmosoftroot/',
+  'https://platzi.com/@gollum23/',
+  'https://platzi.com/@juandc/'
+
+];
+
+const platziMasters = [];
+
+const validateProfile = async (url) => {
   try {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
-    await page.goto('https://platzi.com');
-    await page.screenshot({
-      path: 'src/images/image.png'
-    });
-    await page.pdf({
-      path: 'src/pdfs/website.pdf'
-    });
-    const platziCourseTitle = await page.evaluate(() => document.getElementsByClassName('RecentCourseVideo-content')[0].children[0].innerText
+    await page.goto(url);
+    const validate = await page.evaluate(() =>
+      document.getElementsByTagName('title')[0].innerText.trim()
     );
-    console.log(platziCourseTitle);
-    console.log('Puppeteer End');
-    await browser.close();
-  } catch (error) {
-    console.log(error);
-  }
-}
 
-getDataFromPuppeteer();
+    await browser.close();
+    return validate;
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const getMaster = async (url) => {
+  try {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto(url);
+    const result = await page.evaluate(() => {
+      return {
+        name: document.getElementsByClassName('ProfileHeader-name')[0].innerText,
+        score: document.getElementsByClassName('ProfileScore-number')[0].innerText,
+      };
+    });
+    await browser.close();
+    return result;
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const initializeMasters = async (urls) => {
+  
+
+  await urls.forEach(async (url) => {
+    const isPrivate = await validateProfile(url);
+    if (isPrivate !== 'Cuenta Privada') {
+      const platziMaster = await getMaster(url);
+      platziMasters.push(platziMaster);
+    }
+  });
+
+  
+};
+
+initializeMasters(urls);
+setTimeout(()=>{
+  console.log(platziMasters)
+},25000);
