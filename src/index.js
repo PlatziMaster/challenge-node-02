dfde61d78e62ffd1e114e34af53cd5c9b65deb67
Cristian F. Tovar <cristhiam.tovar@mail.escuelaing.edu.co>
2020-03-20 17:42:30 -0500

    updating

--- src/index.js
+++ src/index.js
@@ -7,17 +7,9 @@ const getDataFromPuppeteer = async (url) => {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
     await page.goto(url);
-    await page.screenshot({
-      path: 'src/images/image.png',
-    });
-    await page.pdf({
-      path: 'src/pdfs/website.pdf',
-    });
-    const price = await page.evaluate(() => document
-      .getElementsByClassName('price-large')[0]
-      .innerText
-      // eslint-disable-next-line comma-dangle
-      .substr(2,));
+
+    const price = await page.evaluate(() => document.getElementsByClassName('price-large')[0].innerText.substr(2,) )
+      console.log(price);
     await browser.close();
     return price;
   } catch (error) {
