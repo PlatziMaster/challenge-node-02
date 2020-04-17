c96b2c57522eb3126a6d809d28683b6d9403464e
AryRosvall <ary.rosvall@gmail.com>
2020-04-16 22:47:43 -0500

    [ADD] login parameters to config

--- src/index.js
+++ src/index.js
@@ -1,34 +1,44 @@
 const prompt = require('prompt');
+const config = require('./config');
 const getDataFromPlatzi = require('./utils/getDataFromPlatzi');
 
-const schema = {
-  properties: {
-    email: {
-      message: 'Ingresa tu correo',
-      required: true,
+const { email, username, password } = config;
+
+if (!email && !username && !password) {
+
+  const schema = {
+    properties: {
+      email: {
+        message: 'Ingresa tu correo',
+        required: true,
+      },
+      password: {
+        hidden: true,
+      },
+      user: {
+        message: 'Ingresa tu usuario Platzi iniciando con @ (Respeta mayúsculas y minúsculas)',
+        required: true,
+      },
     },
-    password: {
-      hidden: true,
-    },
-    user: {
-      message: 'Ingresa tu usuario Platzi iniciando con @ (Respeta mayúsculas y minúsculas)',
-      required: true,
-    },
-  },
-};
+  };
 
-prompt.get(schema, (err, result) => {
-  if (err) {
-    return 1;
-  }
+  prompt.get(schema, (err, result) => {
+    if (err) {
+      throw (err);
+    }
 
-  const { email, password, user } = result;
+    const { email, password, user } = result;
 
-  if (email && password && user) {
-    getDataFromPlatzi(email, password, user);
-  } else {
-    return 1;
-  }
-});
+    if (email && password && user) {
+      process.stdout.write('Analizando...\n');
+      getDataFromPlatzi(email, password, user);
+    }
+  });
 
-prompt.start();
+  prompt.start();
+} else {
+  process.stdout.write('Analizando...\n');
+
+  getDataFromPlatzi(email, password, username);
+}
+
