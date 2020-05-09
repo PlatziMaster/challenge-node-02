dfdad28439e6a9e1dbe41f42d1e70aa9421a0f46
Marco Elizalde <m.elizalde.torres@gmail.com>
2020-05-09 09:09:41 -0500

    se almacena recupera la temperatura

--- src/index.js
+++ src/index.js
@@ -4,15 +4,14 @@ const getDataFromPuppeteer = async () => {
   try {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
-    await page.goto('https://platzi.com');
+    await page.goto('https://weather.com/es-MX/tiempo/hoy/l/20.53,-97.46');
     await page.screenshot({
       path: 'src/images/image.png'
     });
-    await page.pdf({
-      path: 'src/pdfs/website.pdf'
-    });
-    const platziCourseTitle = await page.evaluate(() => document.getElementsByClassName('RecentCourseVideo-content')[0].children[0].innerText
+
+    const platziCourseTitle = await page.evaluate(() => document.getElementsByClassName('today_nowcard-temp')[0].children[0].innerText
     );
+
     console.log(platziCourseTitle);
     console.log('Puppeteer End');
     await browser.close();
