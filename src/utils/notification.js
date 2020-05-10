960079843ad67c5409d4b8bdc86d1b1c579b0199
Marco Elizalde <m.elizalde.torres@gmail.com>
2020-05-10 12:09:14 -0500

    Se integra dotenv para configurar el envio de mensaje

--- src/utils/notification.js
+++ src/utils/notification.js
@@ -1,10 +1,9 @@
-const accountSid = 'accountSid,';
-const authToken = 'authToken,';
-const client = require('twilio')(accountSid, authToken);
+const { config } = require('../config');
+const client = require('twilio')(config.twilioSID, config.twilioToken);
 
 const sendSMS = async (record) => {
     const messaege = await client.messages
-        .create({ body: `Hola la temperatura actual en poza rica es ${record}`, from: '+numOrigin,', to: '+numDestination,' })
+        .create({ body: `Hola la temperatura actual en poza rica es ${record}`, from: `+${config.numOrigin}`, to: `+${config.numDestination}` })
     console.log(messaege.sid);
 }
 
