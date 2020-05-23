811499f5514f3183616a89e8f54c1d80b3482673
SantiagoDuque1700 <duquemoreno12@gmail.com>
2020-05-23 14:19:07 -0500

    reto finalizado

--- src/index.js
+++ src/index.js
@@ -1,24 +1,13 @@
-const puppeteer = require('puppeteer');
+const { CronJob } = require('cron');
+const getData = require('./getData');
 
-const getDataFromPuppeteer = async () => {
-  try {
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    await page.goto('https://platzi.com');
-    await page.screenshot({
-      path: 'src/images/image.png'
-    });
-    await page.pdf({
-      path: 'src/pdfs/website.pdf'
-    });
-    const platziCourseTitle = await page.evaluate(() => document.getElementsByClassName('RecentCourseVideo-content')[0].children[0].innerText
-    );
-    console.log(platziCourseTitle);
-    console.log('Puppeteer End');
-    await browser.close();
-  } catch (error) {
-    console.log(error);
-  }
-}
+getData(true);
 
-getDataFromPuppeteer();
+// eslint-disable-next-line no-console
+console.log('Before job instantiation');
+const job = new CronJob('0 */10 * * * *', (() => {
+  getData(false);
+}));
+// eslint-disable-next-line no-console
+console.log('After job instantiation');
+job.start();
