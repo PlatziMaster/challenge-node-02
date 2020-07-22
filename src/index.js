64d4d9bfdc7a2d5151344963572ae8ec075863fb
Juan Espitia <juanespitia@outlook.com>
2020-07-21 20:22:06 -0500

    Se agrega el Cron Job

--- src/index.js
+++ src/index.js
@@ -1,24 +1,47 @@
+/* eslint-disable no-console */
+const cron = require('node-cron');
+
 const puppeteer = require('puppeteer');
+
+const fs = require('fs');
+
+let mencion = '';
 
 const getDataFromPuppeteer = async () => {
   try {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
-    await page.goto('https://platzi.com');
-    await page.screenshot({
-      path: 'src/images/image.png'
-    });
-    await page.pdf({
-      path: 'src/pdfs/website.pdf'
-    });
-    const platziCourseTitle = await page.evaluate(() => document.getElementsByClassName('RecentCourseVideo-content')[0].children[0].innerText
-    );
-    console.log(platziCourseTitle);
-    console.log('Puppeteer End');
-    await browser.close();
-  } catch (error) {
-    console.log(error);
-  }
-}
 
-getDataFromPuppeteer();
+    fs.readFile('./src/saveArray.txt', 'utf-8', (err, data) => {
+      if (err) {
+        console.log('error: ', err);
+      } else {
+        mencion = data;
+      }
+    });
+
+    await page.goto('https://www.youtube.com/');
+
+    const infoYoutube = await page.evaluate(() => document.getElementsByClassName('ytd-rich-grid-video-renderer')[0].children[1].children[1].children[0].innerText);
+
+    await browser.close();
+
+    if (infoYoutube === mencion) {
+      //console.log('No hay cambios aun');
+    } else {
+      console.log('!!! YA CAMBIO ¡¡¡');
+    }
+
+    fs.writeFile('./src/saveArray.txt', infoYoutube, (err) => {
+      if (err) throw err;
+    });
+
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+cron.schedule('* * * * * ', () => {
+  getDataFromPuppeteer();
+});
+
