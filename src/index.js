f13d7e2fe7c00f0a041b2c8ecacd37c1b9000a4f
oscar <oscar.pay4@gmail.com>
2020-08-27 22:41:11 -0500

    Implement getDataFromPuppeteer

--- src/index.js
+++ src/index.js
@@ -1,24 +1,28 @@
 const puppeteer = require('puppeteer');
+const cron = require('node-cron');
 
 const getDataFromPuppeteer = async () => {
   try {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
-    await page.goto('https://platzi.com');
+    await page.goto('https://blogdesuperheroes.es/');
     await page.screenshot({
-      path: 'src/images/image.png'
+      path: 'src/images/image.png',
     });
     await page.pdf({
-      path: 'src/pdfs/website.pdf'
+      path: 'src/pdfs/website.pdf',
     });
-    const platziCourseTitle = await page.evaluate(() => document.getElementsByClassName('RecentCourseVideo-content')[0].children[0].innerText
-    );
-    console.log(platziCourseTitle);
+    const newsTitle = await page.evaluate(() => document.querySelectorAll('.mnmd-news-ticker__content ul')[0].children[0].querySelector('.post .post__title').innerText);
+    console.log(newsTitle);
     console.log('Puppeteer End');
     await browser.close();
   } catch (error) {
     console.log(error);
   }
-}
+};
 
 getDataFromPuppeteer();
+
+cron.schedule('*/1 * * * *', () => {
+  getDataFromPuppeteer();
+});
